9eb80c07001c42635c8e176fd2469ede8e14e26c
Daimon <daimonssg@gmail.com>
2021-04-14 18:49:54 -0300

    Telas Iniciais do Programa

---
+++ src/utilidade/ciapseular/UtilidadeCiaPSeuLar.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package utilidade.ciapseular;
+/**
+ *
+ * @author Win10
+ */
+public class UtilidadeCiaPSeuLar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
